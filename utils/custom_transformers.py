2a95f14f9157b50920c738ee5c2e5195a4964323
FenryrMKIII <FenryrMKIII@compendium>
2020-06-20 08:20:49 +0200

    Modified utils/custom_transformers.py

--- utils/custom_transformers.py
+++ utils/custom_transformers.py
@@ -96,9 +96,7 @@ class cleaning(BaseEstimator, TransformerMixin):
         self.numeric_, self.non_numeric_ = identify_numeric_from_df(X[col_kept])
         print('columns\n{}\n will be considered as numeric'.format('\n'.join(str(x) for x in self.numeric_)))
         print('columns\n{}\n will be considered as non-numeric'.format('\n'.join(str(x) for x in self.non_numeric_)))
-              
-        self.nan_info_, self.replacements_ = construct_fill_na(self.attribute_filepath, X[col_kept]) # find nan equivalent
-        
+                      
         self.features_names_ = col_kept
                 
         return self
@@ -128,10 +126,6 @@ class cleaning(BaseEstimator, TransformerMixin):
         X.loc[:, self.numeric_], _ = df_to_numeric(X.loc[:, self.numeric_])
         X.loc[:, self.non_numeric_] = X.loc[:, self.non_numeric_].astype('category')
               
-        
-        make_replacement(X, self.replacements_) # make dataframe consistent if multiple nan equivalent for same feature
-        fill_na_presc(X, self.nan_info_) # replace nan equivalent by np.nan
-
         X, self.features_names_ = split_cameo(X, 'CAMEO_INTL_2015')
 
         return X.values
